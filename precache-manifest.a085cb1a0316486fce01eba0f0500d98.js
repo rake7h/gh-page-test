c501107f3939789d189c8e9b5cbc3740fd04c7ae
visualapps <rakesh.singh@mayadata.io>
2019-10-26 18:24:26 +0530

    react-app-test

---
+++ precache-manifest.a085cb1a0316486fce01eba0f0500d98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94b77e109b12ac4a31b3b7edecfc7267",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5417e09dd48a6a3176c7",
+    "url": "/static/css/main.45a950f5.chunk.css"
+  },
+  {
+    "revision": "d7d43920fc57c4d0c8c7",
+    "url": "/static/js/2.b263eed1.chunk.js"
+  },
+  {
+    "revision": "5417e09dd48a6a3176c7",
+    "url": "/static/js/main.e8c5aa0c.chunk.js"
+  },
+  {
+    "revision": "bbc9eb06e8e21625cb93",
+    "url": "/static/js/runtime-main.a8f54343.js"
+  },
+  {
+    "revision": "5fae77e8997230f67fcb2fd929bf27b9",
+    "url": "/static/media/kubeicons.5fae77e8.svg"
+  }
+]);
